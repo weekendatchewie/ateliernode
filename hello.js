3bcba651af7499c9e04029b3796d9b4d3cc464b8
Christopher Poncelet <christopher.poncelet51@gmail.com>
2019-10-02 10:58:27 +0200

    first commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.bold.blue('Anthony'));
+console.log(chalk.red('Valentin'));
+console.log(chalk.bgRed.green('Maxime'));
+console.log(chalk.yellow('Victor'));
